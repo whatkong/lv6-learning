36d865e67bc7b3cb585326f032e5de4f820e1f8c
yuan_mingmin <yuan_mingmin@ionitech.cn>
2019-12-30 16:46:07 +0800

    feat:()ip 黑名单

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Blacklist;
+use App\Http\Middleware\NginxCheck;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -36,7 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            Blacklist::class
+            NginxCheck::class
         ],
 
         'api' => [
